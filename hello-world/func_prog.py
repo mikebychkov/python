8f4f1ee454ecf05413673e3b2b1364b425cedf9b
mikebychkov <mikebychkov@github.com>
2025-07-03 21:31:46 +0500

    update

--- hello-world/func_prog.py
+++ hello-world/func_prog.py
@@ -2,7 +2,8 @@
 
 l = [1,2,3,4,5,6,7,8,9]
 
-###
+
+### MAP, FILTER
 
 def is_even(n):
     return n % 2 == 0
@@ -24,7 +25,7 @@ nl = map(lambda n: n * 2, filter(lambda n: n % 2 == 0, l))
 print(list(nl))
 
 
-########
+######## ZIP
 
 from itertools import zip_longest
 
@@ -45,14 +46,16 @@ z = zip(m3, l)
 x1, x2 = zip(*z) 
 print(x1, x2)
 
-########
+
+######## REDUCE
 
 from functools import reduce
 
 s = reduce(lambda x,y: x + y, l, 0)
 print(s)
 
-########
+
+######## LAMBDA FOR SORTING
 
 list1 = [(4,3),(0,2),(10,-1),(9,9)]
 list1.sort()
@@ -61,6 +64,47 @@ print(list1)
 list1.sort(key=lambda x: x[1])
 print(list1)
 
-########
 
+######## LIST COMPREHENSIONS
+
+hello_list = [char for char in "Hello"]
+print(hello_list)
+
+mult_list = [n * 2 for n in range(10)]
+print(mult_list)
+
+power_list = [n ** 2 for n in range(10) if n % 2 == 0]
+print(power_list)
+
+
+######## SET COMPREHENSIONS (ALL THE SAME BUT {} INSTEAD ())
+
+hello_list = {char for char in "Hello"}
+print(hello_list)
+
+mult_list = {n * 2 for n in range(10)}
+print(mult_list)
+
+power_list = {n ** 2 for n in range(10) if n % 2 == 0}
+print(power_list)
+
+
+######## DICT COMPREHENSIONS
+
+simple_dict = {'a': 1, 'b': 2}
+power_dict = {k:v ** 2 for k, v in simple_dict.items()}
+print(power_dict)
+
+dict2 = {n:n**2 for n in l}
+print(dict2)
+
+
+# Find duplicates
+
+l = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# Result should be ['b', 'n']
+
+dups = {c for c in l if l.count(c) > 1}
+print(dups)
 
