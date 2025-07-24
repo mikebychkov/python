e7283cc5e5a5a57e26075833f59af7a773220a5f
m.bychkov <mikhail.bychkov@octo.uz>
2025-07-24 20:50:13 +0500

    update

---
+++ passchecker/main.py
@@ -0,0 +1,26 @@
+import requests
+import hashlib
+import sys
+
+def sha1_hash(text: str) -> str:
+    sha1 = hashlib.sha1(text.encode('utf-8'))
+    return sha1.hexdigest().upper()
+
+passwd = sys.argv[1]
+
+hash = sha1_hash(passwd)
+
+first5, tail = hash[:5], hash[5:]
+
+url = 'https://api.pwnedpasswords.com/range/' + first5
+
+res = requests.get(url)
+
+print(res)
+
+for sh in res.iter_lines():
+    shs = sh.decode()
+    chstr = shs.split(':')[0]
+    if chstr == tail:
+        print('RESULT:\n', shs)
+
