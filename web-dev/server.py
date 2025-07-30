f45298e430c277f358860c05b540bfcc7f2203fd
m.bychkov <mikhail.bychkov@octo.uz>
2025-07-30 18:12:17 +0500

    update

--- web-dev/server.py
+++ web-dev/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -6,22 +6,32 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/index')
-def home():
-    return render_template('index.html')
+# @app.route('/index')
+# def home():
+#     return render_template('index.html')
+#
+# @app.route('/works')
+# def works():
+#     return render_template('works.html')
+#
+# @app.route('/work')
+# def work():
+#     return render_template('work.html')
+#
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+#
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
 
-@app.route('/works')
-def works():
-    return render_template('works.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(f'{page_name}.html')
 
-@app.route('/work')
-def work():
-    return render_template('work.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
+@app.route('/contact_submit', methods = ['POST'])
 def contact():
-    return render_template('contact.html')
+    form_data = request.form.to_dict()
+    print(form_data)
+    return render_template('thankyou.html')
